6a70b788ad71cba84805170cf3e1e6a99807060b
Sovorn <sovorn@github.com>
2018-11-22 10:39:10 +0700

    Initial Commit

---
+++ Evento/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Evento
+//
+//  Created by Sovorn on 11/22/18.
+//  Copyright © 2018 Sovorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
